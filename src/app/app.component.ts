b0fadf20c0529e1e953e5fb1618fc6947f22d070
shihuiwang <tina.wang@quantropi.com>
2019-05-17 10:19:28 -0400

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-electron';
+
+  max     = 1;
+  current = 0;
+
+   /// finish timer
+  finish() {
+    this.current = this.max;
+  }
+
+  /// reset timer
+  reset() {
+    this.current = 0;
+  }
+
+
+  /// Getters to prevent NaN errors
+
+  get maxVal() {
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return this.currentVal >= this.maxVal;
+  }
+
+
+
 }
